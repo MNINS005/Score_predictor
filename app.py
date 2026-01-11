cfcac225adc6cb759233a63d3dc02aea8a6ea75f
MNINS005 <nikhil06012005@gmail.com>
2026-01-12 01:27:22 +0530

    see

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
-
-
-
 from sklearn.preprocessing import StandardScaler
-
-
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
@@ -46,5 +41,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run()        
+    app.run(host='8080')        
 
